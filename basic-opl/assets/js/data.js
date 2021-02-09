7a6ec360e586f5c049b2fa010af4ea2481dc511a
Shpend Palushi <palushishpend@gmail.com>
2021-02-09 23:13:23 +0100

    commit

--- basic-opl/assets/js/data.js
+++ basic-opl/assets/js/data.js
@@ -1,4 +1,5 @@
-$(document).ready(function(){
-
+jQuery(document).ready(function($){
+    $('select').select2({width:100});
+    $('b[role="presentation"]').hide();
+    $('.select2-selection__arrow').append('<i class="fa fa-angle-down"></i>');
 });
-$(".select2").select2();
